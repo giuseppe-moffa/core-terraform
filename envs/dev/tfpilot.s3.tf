ba89a0ec693d75e17fc23618670ed085f03b49e8
giuseppe-moffa <72092762+giuseppe-moffa@users.noreply.github.com>
2026-02-11 16:35:30 +0000

    chore: infra request req_665063

--- envs/dev/tfpilot.s3.tf
+++ envs/dev/tfpilot.s3.tf
@@ -1,1 +1,11 @@
 # Managed by TfPilot - do not edit by hand
+# --- tfpilot:begin:req_665063 ---
+module "s3-bucket" {
+  source = "../modules/s3-bucket"
+  bucket_name = "dev-assets"
+  acl = "public"
+  versioning = "enabled"
+  encryption = "sse-s3"
+  region = "eu-west-2"
+}
+# --- tfpilot:end:req_665063 ---
