b0b2794343d2729376258edca77b998fe2247c8c
giuseppe-moffa <72092762+giuseppe-moffa@users.noreply.github.com>
2026-02-11 15:22:06 +0000

    chore: infra request req_C2A6D1

---
+++ envs/dev/tfpilot.s3.tf
@@ -0,0 +1,12 @@
+# Managed by TfPilot - do not edit by hand.
+
+# --- tfpilot:begin:req_C2A6D1 ---
+module "s3-bucket" {
+  source = "../modules/s3-bucket"
+  bucket_name = "dev-assets"
+  acl = "public"
+  versioning = "enabled"
+  encryption = "sse-s3"
+  region = "eu-west-2"
+}
+# --- tfpilot:end:req_C2A6D1 ---
