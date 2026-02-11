c279fef0494831a1c67a25fac3fa1679c3e8d2a1
giuseppe-moffa <72092762+giuseppe-moffa@users.noreply.github.com>
2026-02-11 21:55:53 +0000

    chore: infra request 3fdf1ea9-5dfc-455e-8e5b-03d40a38d119

--- envs/dev/tfpilot.s3.tf
+++ envs/dev/tfpilot.s3.tf
@@ -1,2 +1,19 @@
 # Managed by TfPilot - do not edit by hand
 # Placeholder removed: example module was provisioning test resources.
+# --- tfpilot:begin:3fdf1ea9-5dfc-455e-8e5b-03d40a38d119 ---
+module "tfpilot_3fdf1ea9_5dfc_455e_8e5b_03d40a38d119" {
+  source = "../../modules/s3-bucket"
+  bucket_name = "dev-assets-s3"
+  versioning_enabled = true
+  name = "dev-assets-s3"
+  project = "core"
+  environment = "dev"
+  request_id = "3fdf1ea9-5dfc-455e-8e5b-03d40a38d119"
+  tags = {
+    ManagedBy = "tfpilot"
+    TfPilotRequestId = "3fdf1ea9-5dfc-455e-8e5b-03d40a38d119"
+    Project = "core"
+    Environment = "dev"
+  }
+}
+# --- tfpilot:end:3fdf1ea9-5dfc-455e-8e5b-03d40a38d119 ---
