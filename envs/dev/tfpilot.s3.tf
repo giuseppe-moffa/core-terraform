22e495e83ef4151d3f9ea85f23bcb84ca7bbcffb
giuseppe-moffa <72092762+giuseppe-moffa@users.noreply.github.com>
2026-02-11 22:33:48 +0000

    chore: infra request req_dev_s3_NG9SXP

--- envs/dev/tfpilot.s3.tf
+++ envs/dev/tfpilot.s3.tf
@@ -1,2 +1,19 @@
 # Managed by TfPilot - do not edit by hand
 # Placeholder removed: example module was provisioning test resources.
+# --- tfpilot:begin:req_dev_s3_NG9SXP ---
+module "tfpilot_req_dev_s3_NG9SXP" {
+  source = "../../modules/s3-bucket"
+  bucket_name = "dev-s3-bucket-new"
+  versioning_enabled = true
+  name = "dev-s3-bucket-new"
+  project = "core"
+  environment = "dev"
+  request_id = "req_dev_s3_NG9SXP"
+  tags = {
+    ManagedBy = "tfpilot"
+    TfPilotRequestId = "req_dev_s3_NG9SXP"
+    Project = "core"
+    Environment = "dev"
+  }
+}
+# --- tfpilot:end:req_dev_s3_NG9SXP ---
