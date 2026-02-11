294f20a79255641a81c0992573653192a10d8a51
giuseppe-moffa <72092762+giuseppe-moffa@users.noreply.github.com>
2026-02-11 22:42:24 +0000

    chore: infra request req_dev_s3_23T64M

--- envs/dev/tfpilot.s3.tf
+++ envs/dev/tfpilot.s3.tf
@@ -1,2 +1,19 @@
 # Managed by TfPilot - do not edit by hand
 # Placeholder removed: example module was provisioning test resources.
+# --- tfpilot:begin:req_dev_s3_23T64M ---
+module "tfpilot_req_dev_s3_23T64M" {
+  source = "../../modules/s3-bucket"
+  bucket_name = "dev-s3-bucket-wtp"
+  versioning_enabled = true
+  name = "dev-s3-bucket-wtp"
+  project = "core"
+  environment = "dev"
+  request_id = "req_dev_s3_23T64M"
+  tags = {
+    ManagedBy = "tfpilot"
+    TfPilotRequestId = "req_dev_s3_23T64M"
+    Project = "core"
+    Environment = "dev"
+  }
+}
+# --- tfpilot:end:req_dev_s3_23T64M ---
