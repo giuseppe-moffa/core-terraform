8e1a1214d1d8a0ee395539cd4905680941c9577f
giuseppe-moffa <72092762+giuseppe-moffa@users.noreply.github.com>
2026-02-11 19:54:27 +0000

    chore: infra request 01385fb1-e37a-4c02-b0cc-4500cf362809

--- envs/dev/tfpilot.s3.tf
+++ envs/dev/tfpilot.s3.tf
@@ -17,3 +17,20 @@ module "tfpilot_request_example_s3" {
   }
 }
 /* --- tfpilot example s3 end --- */
+# --- tfpilot:begin:01385fb1-e37a-4c02-b0cc-4500cf362809 ---
+module "tfpilot_01385fb1_e37a_4c02_b0cc_4500cf362809" {
+  source = "../../modules/s3-bucket"
+  bucket_name = "dev-assets"
+  versioning_enabled = true
+  tags = {
+    ManagedBy = "tfpilot"
+    TfPilotRequestId = "01385fb1-e37a-4c02-b0cc-4500cf362809"
+    Project = "core"
+    Environment = "dev"
+  }
+  name = "dev-assets"
+  project = "core"
+  environment = "dev"
+  request_id = "01385fb1-e37a-4c02-b0cc-4500cf362809"
+}
+# --- tfpilot:end:01385fb1-e37a-4c02-b0cc-4500cf362809 ---
