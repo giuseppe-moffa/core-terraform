2254a86cdb1dc50983bb3594a6aaeafa56aa31ba
giuseppe-moffa <72092762+giuseppe-moffa@users.noreply.github.com>
2026-02-11 18:15:15 +0000

    chore: infra request req_BAE482

--- envs/dev/tfpilot.s3.tf
+++ envs/dev/tfpilot.s3.tf
@@ -17,3 +17,17 @@ module "tfpilot_request_example_s3" {
   }
 }
 /* --- tfpilot example s3 end --- */
+# --- tfpilot:begin:req_BAE482 ---
+module "s3-bucket" {
+  source = "../../modules/s3-bucket"
+  bucket_name = "dev-assets"
+  region = "eu-west-2"
+  acl = "public"
+  versioning = "enabled"
+  encryption_enabled = true
+  encryption_type = "SSE-S3"
+  block_public_access = false
+  access_logging = false
+  tags = jsonencode({"service":"assets"})
+}
+# --- tfpilot:end:req_BAE482 ---
