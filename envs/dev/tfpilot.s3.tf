25548cbe3fae81dd107f0137dc42655414e49acc
giuseppe-moffa <72092762+giuseppe-moffa@users.noreply.github.com>
2026-02-11 23:07:36 +0000

    chore: infra request req_dev_s3_AHGXZX

--- envs/dev/tfpilot.s3.tf
+++ envs/dev/tfpilot.s3.tf
@@ -1,2 +1,19 @@
 # Managed by TfPilot - do not edit by hand
 # Placeholder removed: example module was provisioning test resources.
+# --- tfpilot:begin:req_dev_s3_AHGXZX ---
+module "tfpilot_req_dev_s3_AHGXZX" {
+  source = "../../modules/s3-bucket"
+  bucket_name = "dev-s3-new-26"
+  versioning_enabled = true
+  name = "dev-s3-new-26"
+  project = "core"
+  environment = "dev"
+  request_id = "req_dev_s3_AHGXZX"
+  tags = {
+    ManagedBy = "tfpilot"
+    TfPilotRequestId = "req_dev_s3_AHGXZX"
+    Project = "core"
+    Environment = "dev"
+  }
+}
+# --- tfpilot:end:req_dev_s3_AHGXZX ---
