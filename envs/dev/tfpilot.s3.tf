f734cfb3bbf7ae7cf151075c0179a5e2dd1b9beb
giuseppe-moffa <72092762+giuseppe-moffa@users.noreply.github.com>
2026-02-11 18:47:33 +0000

    chore: infra request req_5973E8

--- envs/dev/tfpilot.s3.tf
+++ envs/dev/tfpilot.s3.tf
@@ -17,3 +17,15 @@ module "tfpilot_request_example_s3" {
   }
 }
 /* --- tfpilot example s3 end --- */
+# --- tfpilot:begin:req_5973E8 ---
+module "s3-bucket" {
+  source = "../../modules/s3-bucket"
+  bucket_name = "dev-assets"
+  region = "eu-west-2"
+  acl = "public"
+  versioning_enabled = true
+  encryption_enabled = true
+  encryption_type = "SSE-S3"
+  block_public_access = false
+}
+# --- tfpilot:end:req_5973E8 ---
