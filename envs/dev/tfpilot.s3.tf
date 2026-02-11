9f29ee04614ddd0c511c77f078fa8f1d67b3e96a
giuseppe-moffa <72092762+giuseppe-moffa@users.noreply.github.com>
2026-02-11 23:02:07 +0000

    chore: infra request req_dev_s3_G9D5AK

--- envs/dev/tfpilot.s3.tf
+++ envs/dev/tfpilot.s3.tf
@@ -1,2 +1,19 @@
 # Managed by TfPilot - do not edit by hand
 # Placeholder removed: example module was provisioning test resources.
+# --- tfpilot:begin:req_dev_s3_G9D5AK ---
+module "tfpilot_req_dev_s3_G9D5AK" {
+  source = "../../modules/s3-bucket"
+  bucket_name = "dev-s3-wtp"
+  versioning_enabled = true
+  name = "dev-s3-wtp"
+  project = "core"
+  environment = "dev"
+  request_id = "req_dev_s3_G9D5AK"
+  tags = {
+    ManagedBy = "tfpilot"
+    TfPilotRequestId = "req_dev_s3_G9D5AK"
+    Project = "core"
+    Environment = "dev"
+  }
+}
+# --- tfpilot:end:req_dev_s3_G9D5AK ---
