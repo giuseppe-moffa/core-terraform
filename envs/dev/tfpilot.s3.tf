48a5032c75189f1a9992879ba370b28c19feb142
giuseppe-moffa <72092762+giuseppe-moffa@users.noreply.github.com>
2026-02-11 23:21:24 +0000

    chore: infra request req_dev_s3_8DUBDG

--- envs/dev/tfpilot.s3.tf
+++ envs/dev/tfpilot.s3.tf
@@ -1,2 +1,19 @@
 # Managed by TfPilot - do not edit by hand
 # Placeholder removed: example module was provisioning test resources.
+# --- tfpilot:begin:req_dev_s3_8DUBDG ---
+module "tfpilot_req_dev_s3_8DUBDG" {
+  source = "../../modules/s3-bucket"
+  bucket_name = "dev-s3-new"
+  versioning_enabled = true
+  name = "dev-s3-new"
+  project = "core"
+  environment = "dev"
+  request_id = "req_dev_s3_8DUBDG"
+  tags = {
+    ManagedBy = "tfpilot"
+    TfPilotRequestId = "req_dev_s3_8DUBDG"
+    Project = "core"
+    Environment = "dev"
+  }
+}
+# --- tfpilot:end:req_dev_s3_8DUBDG ---
