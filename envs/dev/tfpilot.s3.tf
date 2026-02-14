7c663ae1b909ad3997ac9df57e8e40dfe2986f3a
giuseppe-moffa <72092762+giuseppe-moffa@users.noreply.github.com>
2026-02-14 23:24:28 +0000

    chore: infra request req_dev_s3_KR7LPF

--- envs/dev/tfpilot.s3.tf
+++ envs/dev/tfpilot.s3.tf
@@ -1,2 +1,21 @@
 # Managed by TfPilot - do not edit by hand
 
+# --- tfpilot:begin:req_dev_s3_KR7LPF ---
+module "tfpilot_req_dev_s3_KR7LPF" {
+  source = "../../modules/s3-bucket"
+  name = "core-public-s3-assets-KR7LPF"
+  versioning_enabled = false
+  block_public_access = false
+  enable_lifecycle = true
+  encryption_mode = "sse-s3"
+  project = "core"
+  environment = "dev"
+  request_id = "req_dev_s3_KR7LPF"
+  tags = {
+    ManagedBy = "tfpilot"
+    TfPilotRequestId = "req_dev_s3_KR7LPF"
+    Project = "core"
+    Environment = "dev"
+  }
+}
+# --- tfpilot:end:req_dev_s3_KR7LPF ---
