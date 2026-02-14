0bcb988711ccb52db8a72d4f01ba5c48444cd315
giuseppe-moffa <72092762+giuseppe-moffa@users.noreply.github.com>
2026-02-14 13:51:16 +0000

    chore: infra request req_dev_s3_L5ZN56

--- envs/dev/tfpilot.s3.tf
+++ envs/dev/tfpilot.s3.tf
@@ -1,2 +1,22 @@
 # Managed by TfPilot - do not edit by hand
 
+# --- tfpilot:begin:req_dev_s3_L5ZN56 ---
+module "tfpilot_req_dev_s3_L5ZN56" {
+  source = "../../modules/s3-bucket"
+  name = "core-dev-private-assets-L5ZN56"
+  block_public_access = true
+  versioning_enabled = false
+  sse_s3_enabled = true
+  access_logging_enabled = false
+  enable_lifecycle = true
+  project = "core"
+  environment = "dev"
+  request_id = "req_dev_s3_L5ZN56"
+  tags = {
+    ManagedBy = "tfpilot"
+    TfPilotRequestId = "req_dev_s3_L5ZN56"
+    Project = "core"
+    Environment = "dev"
+  }
+}
+# --- tfpilot:end:req_dev_s3_L5ZN56 ---
