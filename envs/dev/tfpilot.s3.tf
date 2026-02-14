7b1f6bc18dfeca3586c9d36b855206cbf11caa16
giuseppe-moffa <72092762+giuseppe-moffa@users.noreply.github.com>
2026-02-14 01:23:31 +0000

    chore: infra request req_dev_s3_4T837A

--- envs/dev/tfpilot.s3.tf
+++ envs/dev/tfpilot.s3.tf
@@ -1,2 +1,21 @@
 # Managed by TfPilot - do not edit by hand
 
+# --- tfpilot:begin:req_dev_s3_4T837A ---
+module "tfpilot_req_dev_s3_4T837A" {
+  source = "../../modules/s3-bucket"
+  bucket_name = "tfpilot-dev-web-assets"
+  versioning_enabled = false
+  block_public_access = false
+  enable_lifecycle = true
+  name = "tfpilot-dev-web-assets"
+  project = "core"
+  environment = "dev"
+  request_id = "req_dev_s3_4T837A"
+  tags = {
+    ManagedBy = "tfpilot"
+    TfPilotRequestId = "req_dev_s3_4T837A"
+    Project = "core"
+    Environment = "dev"
+  }
+}
+# --- tfpilot:end:req_dev_s3_4T837A ---
