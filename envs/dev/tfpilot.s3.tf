e040c8d006a1b26cf8c8ea70810bc588e4b9f38d
giuseppe-moffa <72092762+giuseppe-moffa@users.noreply.github.com>
2026-02-14 12:54:28 +0000

    chore: infra request req_dev_s3_QUUQ5L

--- envs/dev/tfpilot.s3.tf
+++ envs/dev/tfpilot.s3.tf
@@ -1,2 +1,21 @@
 # Managed by TfPilot - do not edit by hand
 
+# --- tfpilot:begin:req_dev_s3_QUUQ5L ---
+module "tfpilot_req_dev_s3_QUUQ5L" {
+  source = "../../modules/s3-bucket"
+  name = "tfp-core-dev-s3-req-dev-s3-QUUQ5L"
+  versioning_enabled = true
+  block_public_access = true
+  force_destroy = false
+  enable_lifecycle = true
+  project = "core"
+  environment = "dev"
+  request_id = "req_dev_s3_QUUQ5L"
+  tags = {
+    ManagedBy = "tfpilot"
+    TfPilotRequestId = "req_dev_s3_QUUQ5L"
+    Project = "core"
+    Environment = "dev"
+  }
+}
+# --- tfpilot:end:req_dev_s3_QUUQ5L ---
