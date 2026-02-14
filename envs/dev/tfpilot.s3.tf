6837112a5d00491ad94f581970bde6b4083f1a19
giuseppe-moffa <72092762+giuseppe-moffa@users.noreply.github.com>
2026-02-14 13:44:33 +0000

    chore: infra request req_dev_s3_RSSJTG

--- envs/dev/tfpilot.s3.tf
+++ envs/dev/tfpilot.s3.tf
@@ -1,2 +1,23 @@
 # Managed by TfPilot - do not edit by hand
 
+# --- tfpilot:begin:req_dev_s3_RSSJTG ---
+module "tfpilot_req_dev_s3_RSSJTG" {
+  source = "../../modules/s3-bucket"
+  name = "tfp-core-dev-public-assets-RSSJTG"
+  public_read = true
+  versioning_enabled = false
+  default_encryption_enabled = true
+  block_public_access = false
+  access_logging_enabled = false
+  enable_lifecycle = true
+  project = "core"
+  environment = "dev"
+  request_id = "req_dev_s3_RSSJTG"
+  tags = {
+    ManagedBy = "tfpilot"
+    TfPilotRequestId = "req_dev_s3_RSSJTG"
+    Project = "core"
+    Environment = "dev"
+  }
+}
+# --- tfpilot:end:req_dev_s3_RSSJTG ---
