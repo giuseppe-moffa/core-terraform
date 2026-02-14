fc32f5a6adb74e8b0f89b91eff7d378e47968c01
giuseppe-moffa <72092762+giuseppe-moffa@users.noreply.github.com>
2026-02-14 00:30:44 +0000

    chore: infra request req_dev_s3_ART435

--- envs/dev/tfpilot.s3.tf
+++ envs/dev/tfpilot.s3.tf
@@ -1,2 +1,21 @@
 # Managed by TfPilot - do not edit by hand
 
+# --- tfpilot:begin:req_dev_s3_ART435 ---
+module "tfpilot_req_dev_s3_ART435" {
+  source = "../../modules/s3-bucket"
+  bucket_name = "tfpilot-dev-public-assets-bucket"
+  versioning_enabled = false
+  block_public_access = false
+  enable_lifecycle = true
+  name = "tfpilot-dev-public-assets-bucket"
+  project = "core"
+  environment = "dev"
+  request_id = "req_dev_s3_ART435"
+  tags = {
+    ManagedBy = "tfpilot"
+    TfPilotRequestId = "req_dev_s3_ART435"
+    Project = "core"
+    Environment = "dev"
+  }
+}
+# --- tfpilot:end:req_dev_s3_ART435 ---
