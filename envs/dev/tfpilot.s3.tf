fa399b822b083b6aabd7034375029628372bbfea
giuseppe-moffa <72092762+giuseppe-moffa@users.noreply.github.com>
2026-02-15 00:03:34 +0000

    chore: infra request req_dev_s3_CXBZ3W

--- envs/dev/tfpilot.s3.tf
+++ envs/dev/tfpilot.s3.tf
@@ -1,2 +1,21 @@
 # Managed by TfPilot - do not edit by hand
 
+# --- tfpilot:begin:req_dev_s3_CXBZ3W ---
+module "tfpilot_req_dev_s3_CXBZ3W" {
+  source = "../../modules/s3-bucket"
+  name = "core-s3-private-CXBZ3W"
+  versioning_enabled = false
+  block_public_access = true
+  enable_lifecycle = true
+  encryption_mode = "sse-s3"
+  project = "core"
+  environment = "dev"
+  request_id = "req_dev_s3_CXBZ3W"
+  tags = {
+    ManagedBy = "tfpilot"
+    TfPilotRequestId = "req_dev_s3_CXBZ3W"
+    Project = "core"
+    Environment = "dev"
+  }
+}
+# --- tfpilot:end:req_dev_s3_CXBZ3W ---
