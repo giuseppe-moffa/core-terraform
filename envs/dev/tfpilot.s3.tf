0fef559f2fd9922398f43a609192a3ac60d78970
giuseppe-moffa <72092762+giuseppe-moffa@users.noreply.github.com>
2026-02-15 23:19:37 +0000

    chore: infra request req_dev_s3_Q6VNMB

--- envs/dev/tfpilot.s3.tf
+++ envs/dev/tfpilot.s3.tf
@@ -23,3 +23,26 @@ module "tfpilot_req_dev_s3_G7NFNM" {
   }
 }
 # --- tfpilot:end:req_dev_s3_G7NFNM ---
+# --- tfpilot:begin:req_dev_s3_Q6VNMB ---
+module "tfpilot_req_dev_s3_Q6VNMB" {
+  source = "../../modules/s3-bucket"
+  name = "core-sftp-q6vnmb"
+  project = "core"
+  environment = "dev"
+  request_id = "req_dev_s3_Q6VNMB"
+  bucket_name = "core-sftp-q6vnmb"
+  versioning_enabled = true
+  force_destroy = false
+  block_public_access = true
+  enable_lifecycle = false
+  noncurrent_expiration_days = 30
+  abort_multipart_days = 7
+  encryption_mode = "sse-s3"
+  tags = {
+    ManagedBy = "tfpilot"
+    TfPilotRequestId = "req_dev_s3_Q6VNMB"
+    Project = "core"
+    Environment = "dev"
+  }
+}
+# --- tfpilot:end:req_dev_s3_Q6VNMB ---
