fa7585e90fe86792d43784d0c54f6370f5e8f463
giuseppe-moffa <72092762+giuseppe-moffa@users.noreply.github.com>
2026-02-16 14:56:51 +0000

    chore: infra request req_prod_s3_HXSYCR

--- envs/prod/tfpilot.s3.tf
+++ envs/prod/tfpilot.s3.tf
@@ -1,1 +1,26 @@
 # Managed by TfPilot - do not edit by hand
+# --- tfpilot:begin:req_prod_s3_HXSYCR ---
+module "tfpilot_req_prod_s3_HXSYCR" {
+  source = "../../modules/s3-bucket"
+  name = "core-public-assets-hxsycr"
+  project = "core"
+  environment = "prod"
+  request_id = "req_prod_s3_HXSYCR"
+  bucket_name = "core-public-assets-hxsycr"
+  versioning_enabled = true
+  force_destroy = false
+  block_public_access = false
+  enable_lifecycle = false
+  noncurrent_expiration_days = 30
+  abort_multipart_days = 7
+  encryption_mode = "sse-s3"
+  tags = {
+    ManagedBy = "tfpilot"
+    TfPilotRequestId = "req_prod_s3_HXSYCR"
+    Project = "core"
+    Environment = "prod"
+    data_classification = "public"
+    purpose = "public-assets"
+  }
+}
+# --- tfpilot:end:req_prod_s3_HXSYCR ---
