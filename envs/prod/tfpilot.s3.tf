5bdef5d01ca8ba0376bf942fb38e437bd06d52dc
giuseppe-moffa <72092762+giuseppe-moffa@users.noreply.github.com>
2026-02-21 22:56:00 +0000

    chore: infra request req_prod_s3_2PMLFN

--- envs/prod/tfpilot.s3.tf
+++ envs/prod/tfpilot.s3.tf
@@ -1,1 +1,24 @@
 # Managed by TfPilot - do not edit by hand
+# --- tfpilot:begin:req_prod_s3_2PMLFN ---
+module "tfpilot_req_prod_s3_2PMLFN" {
+  source = "../../modules/s3-bucket"
+  name = "core-assets-1-2pmlfn"
+  project = "core"
+  environment = "prod"
+  request_id = "req_prod_s3_2PMLFN"
+  bucket_name = "core-assets-1-2pmlfn"
+  versioning_enabled = true
+  force_destroy = false
+  block_public_access = true
+  enable_lifecycle = false
+  noncurrent_expiration_days = 30
+  abort_multipart_days = 7
+  encryption_mode = "sse-s3"
+  tags = {
+    ManagedBy = "tfpilot"
+    TfPilotRequestId = "req_prod_s3_2PMLFN"
+    Project = "core"
+    Environment = "prod"
+  }
+}
+# --- tfpilot:end:req_prod_s3_2PMLFN ---
