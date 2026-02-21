13bcb1cadd3af0e4a47004889a3e18221c5e9c82
giuseppe-moffa <72092762+giuseppe-moffa@users.noreply.github.com>
2026-02-21 20:13:21 +0000

    chore: infra request req_dev_s3_KQVUBJ

--- envs/dev/tfpilot.s3.tf
+++ envs/dev/tfpilot.s3.tf
@@ -25,3 +25,28 @@ module "tfpilot_req_dev_s3_RCL5JW" {
   }
 }
 # --- tfpilot:end:req_dev_s3_RCL5JW ---
+# --- tfpilot:begin:req_dev_s3_KQVUBJ ---
+module "tfpilot_req_dev_s3_KQVUBJ" {
+  source = "../../modules/s3-bucket"
+  name = "core-private-assets-kqvubj"
+  project = "core"
+  environment = "dev"
+  request_id = "req_dev_s3_KQVUBJ"
+  bucket_name = "core-private-assets-kqvubj"
+  versioning_enabled = true
+  force_destroy = false
+  block_public_access = true
+  enable_lifecycle = true
+  noncurrent_expiration_days = 30
+  abort_multipart_days = 7
+  encryption_mode = "sse-s3"
+  tags = {
+    ManagedBy = "tfpilot"
+    TfPilotRequestId = "req_dev_s3_KQVUBJ"
+    Project = "core"
+    Environment = "dev"
+    purpose = "assets"
+    data_classification = "private"
+  }
+}
+# --- tfpilot:end:req_dev_s3_KQVUBJ ---
