cfea8edf7665cc1e5d391f9f1678de0d36748011
giuseppe-moffa <72092762+giuseppe-moffa@users.noreply.github.com>
2026-02-21 23:11:47 +0000

    chore: infra request req_dev_s3_9JPX57

--- envs/dev/tfpilot.s3.tf
+++ envs/dev/tfpilot.s3.tf
@@ -1,1 +1,24 @@
 # Managed by TfPilot - do not edit by hand
+# --- tfpilot:begin:req_dev_s3_9JPX57 ---
+module "tfpilot_req_dev_s3_9JPX57" {
+  source = "../../modules/s3-bucket"
+  name = "core-assets-9jpx57"
+  project = "core"
+  environment = "dev"
+  request_id = "req_dev_s3_9JPX57"
+  bucket_name = "core-assets-9jpx57"
+  versioning_enabled = true
+  force_destroy = false
+  block_public_access = true
+  enable_lifecycle = false
+  noncurrent_expiration_days = 30
+  abort_multipart_days = 7
+  encryption_mode = "sse-s3"
+  tags = {
+    ManagedBy = "tfpilot"
+    TfPilotRequestId = "req_dev_s3_9JPX57"
+    Project = "core"
+    Environment = "dev"
+  }
+}
+# --- tfpilot:end:req_dev_s3_9JPX57 ---
