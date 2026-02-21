c4da2696bd6233b541e5676a1d40ae241c77a5dc
giuseppe-moffa <72092762+giuseppe-moffa@users.noreply.github.com>
2026-02-21 23:23:24 +0000

    Merge main into request/req_dev_s3_9QBWXG (resolve conflicts - accept both)

--- envs/dev/tfpilot.s3.tf
+++ envs/dev/tfpilot.s3.tf
@@ -22,3 +22,26 @@ module "tfpilot_req_dev_s3_UPBBXG" {
   }
 }
 # --- tfpilot:end:req_dev_s3_UPBBXG ---
+# --- tfpilot:begin:req_dev_s3_9QBWXG ---
+module "tfpilot_req_dev_s3_9QBWXG" {
+  source = "../../modules/s3-bucket"
+  name = "core-assets-5-9qbwxg"
+  project = "core"
+  environment = "dev"
+  request_id = "req_dev_s3_9QBWXG"
+  bucket_name = "core-assets-5-9qbwxg"
+  versioning_enabled = true
+  force_destroy = false
+  block_public_access = true
+  enable_lifecycle = false
+  noncurrent_expiration_days = 30
+  abort_multipart_days = 7
+  encryption_mode = "sse-s3"
+  tags = {
+    ManagedBy = "tfpilot"
+    TfPilotRequestId = "req_dev_s3_9QBWXG"
+    Project = "core"
+    Environment = "dev"
+  }
+}
+# --- tfpilot:end:req_dev_s3_9QBWXG ---
