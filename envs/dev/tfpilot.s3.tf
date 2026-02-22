e23bbacba269defe9d4368408fab5582bdfa7cde
giuseppe-moffa <72092762+giuseppe-moffa@users.noreply.github.com>
2026-02-22 12:36:14 +0000

    chore: infra request req_dev_s3_XHYBWK

--- envs/dev/tfpilot.s3.tf
+++ envs/dev/tfpilot.s3.tf
@@ -45,3 +45,31 @@ module "tfpilot_req_dev_s3_53Y2LJ" {
   }
 }
 # --- tfpilot:end:req_dev_s3_53Y2LJ ---
+# --- tfpilot:begin:req_dev_s3_XHYBWK ---
+module "tfpilot_req_dev_s3_XHYBWK" {
+  source = "../../modules/s3-bucket"
+  name = "core-public-assets-xhybwk"
+  project = "core"
+  environment = "dev"
+  request_id = "req_dev_s3_XHYBWK"
+  bucket_name = "core-public-assets-xhybwk"
+  versioning_enabled = false
+  force_destroy = true
+  block_public_access = true
+  enable_lifecycle = false
+  noncurrent_expiration_days = 30
+  abort_multipart_days = 7
+  encryption_mode = "sse-s3"
+  tags = {
+    ManagedBy = "tfpilot"
+    TfPilotRequestId = "req_dev_s3_XHYBWK"
+    Project = "core"
+    Environment = "dev"
+    tfpilot:request_id = "req_dev_s3_XHYBWK"
+    tfpilot:project = "core"
+    tfpilot:environment = "dev"
+    tfpilot:created_by = "giuseppe-moffa"
+    tfpilot:template_id = "s3-public-assets"
+  }
+}
+# --- tfpilot:end:req_dev_s3_XHYBWK ---
