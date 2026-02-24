a89b4436706f875fa725ee5542d855196e21067a
giuseppe-moffa <72092762+giuseppe-moffa@users.noreply.github.com>
2026-02-24 09:57:24 +0000

    chore: infra request req_dev_s3_nwpc75

--- envs/dev/tfpilot.s3.tf
+++ envs/dev/tfpilot.s3.tf
@@ -54,3 +54,30 @@ module "tfpilot_req_dev_s3_uwsdwe" {
   }
 }
 # --- tfpilot:end:req_dev_s3_uwsdwe ---
+# --- tfpilot:begin:req_dev_s3_nwpc75 ---
+module "tfpilot_req_dev_s3_nwpc75" {
+  source = "../../modules/s3-bucket"
+  name = "ai-agent-nwpc75"
+  project = "core"
+  environment = "dev"
+  request_id = "req_dev_s3_nwpc75"
+  versioning_enabled = false
+  force_destroy = true
+  block_public_access = true
+  enable_lifecycle = false
+  noncurrent_expiration_days = 30
+  abort_multipart_days = 7
+  encryption_mode = "sse-s3"
+  tags = {
+    ManagedBy = "tfpilot"
+    TfPilotRequestId = "req_dev_s3_nwpc75"
+    Project = "core"
+    Environment = "dev"
+    "tfpilot:request_id" = "req_dev_s3_nwpc75"
+    "tfpilot:project" = "core"
+    "tfpilot:environment" = "dev"
+    "tfpilot:created_by" = "giuseppe-moffa"
+    "tfpilot:template_id" = "s3-public-assets"
+  }
+}
+# --- tfpilot:end:req_dev_s3_nwpc75 ---
