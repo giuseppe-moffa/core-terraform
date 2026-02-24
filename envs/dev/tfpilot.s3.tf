4b64032319b5895851ab3ea55e8b87ccb13921c8
giuseppe-moffa <72092762+giuseppe-moffa@users.noreply.github.com>
2026-02-24 02:04:32 +0000

    Update tfpilot.s3.tf

--- envs/dev/tfpilot.s3.tf
+++ envs/dev/tfpilot.s3.tf
@@ -27,31 +27,3 @@ module "tfpilot_req_dev_s3_fxr3yn" {
   }
 }
 # --- tfpilot:end:req_dev_s3_fxr3yn ---
-# --- tfpilot:begin:req_dev_s3_d96pq8 ---
-module "tfpilot_req_dev_s3_d96pq8" {
-  source = "../../modules/s3-bucket"
-  name = "ai-agent-mk9vmv-d96pq8"
-  project = "core"
-  environment = "dev"
-  request_id = "req_dev_s3_d96pq8"
-  bucket_name = "ai-agent-mk9vmv-d96pq8"
-  versioning_enabled = false
-  force_destroy = true
-  block_public_access = true
-  enable_lifecycle = false
-  noncurrent_expiration_days = 30
-  abort_multipart_days = 7
-  encryption_mode = "sse-s3"
-  tags = {
-    ManagedBy = "tfpilot"
-    TfPilotRequestId = "req_dev_s3_d96pq8"
-    Project = "core"
-    Environment = "dev"
-    "tfpilot:request_id" = "req_dev_s3_d96pq8"
-    "tfpilot:project" = "core"
-    "tfpilot:environment" = "dev"
-    "tfpilot:created_by" = "giuseppe-moffa"
-    "tfpilot:template_id" = "s3-public-assets"
-  }
-}
-# --- tfpilot:end:req_dev_s3_d96pq8 ---
