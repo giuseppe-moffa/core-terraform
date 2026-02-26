5c22f7e0f41b8bd4113e0b9001983f2711f07fde
Giuseppe Moffa <giuseppemoff10@gmail.com>
2026-02-26 19:21:47 +0000

    wip

--- envs/dev/backend.tf
+++ envs/dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "tfpilot-tfstate-core-dev"
-    key            = "tfpilot-tfstate-core-dev/envs/dev/terraform.tfstate"
+    key            = "terraform.tfstate"
     region         = "eu-west-2"
     dynamodb_table = "tfpilot-tfstate-lock-core-dev"
     encrypt        = true
