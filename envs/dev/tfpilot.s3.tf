f3182be64b618985c1e3a83856cf8487bd3785df
giuseppe-moffa <72092762+giuseppe-moffa@users.noreply.github.com>
2026-02-27 20:44:50 +0000

    chore: infra request req_dev_s3_2tf9av

--- envs/dev/tfpilot.s3.tf
+++ envs/dev/tfpilot.s3.tf
@@ -1,1 +1,28 @@
 # Managed by TfPilot - do not edit by hand
+# --- tfpilot:begin:req_dev_s3_2tf9av ---
+module "tfpilot_req_dev_s3_2tf9av" {
+  source = "../../modules/s3-bucket"
+  name = "core-dev-ai-agent-2tf9av"
+  project = "core"
+  environment = "dev"
+  request_id = "req_dev_s3_2tf9av"
+  versioning_enabled = false
+  force_destroy = true
+  block_public_access = true
+  enable_lifecycle = false
+  noncurrent_expiration_days = 30
+  abort_multipart_days = 7
+  encryption_mode = "sse-s3"
+  tags = {
+    ManagedBy = "tfpilot"
+    TfPilotRequestId = "req_dev_s3_2tf9av"
+    Project = "core"
+    Environment = "dev"
+    "tfpilot:request_id" = "req_dev_s3_2tf9av"
+    "tfpilot:project" = "core"
+    "tfpilot:environment" = "dev"
+    "tfpilot:created_by" = "giuseppe-moffa"
+    "tfpilot:template_id" = "s3-public-assets"
+  }
+}
+# --- tfpilot:end:req_dev_s3_2tf9av ---
